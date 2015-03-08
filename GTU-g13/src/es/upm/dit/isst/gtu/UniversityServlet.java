5149377ee831c38ffd6b228996f2cd9459e4b6db
jaimeveg <jaimevegas@msn.com>
2015-03-08 12:15:08 +0100

    Creados servlets para entidades del sistema GTU

---
+++ GTU-g13/src/es/upm/dit/isst/gtu/UniversityServlet.java
@@ -0,0 +1,17 @@
+package es.upm.dit.isst.gtu;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UniversityServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello University");
+	}
+
+}
