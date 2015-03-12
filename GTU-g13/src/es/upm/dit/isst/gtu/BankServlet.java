3a120c0252d24b5dcf62c5fd69d99585efeb4e5c
jaimeveg <jaimevegas@msn.com>
2015-03-12 18:55:11 +0100

    Agregada logica de tramitacion del Banco

--- GTU-g13/src/es/upm/dit/isst/gtu/BankServlet.java
+++ GTU-g13/src/es/upm/dit/isst/gtu/BankServlet.java
@@ -1,6 +1,8 @@
 package es.upm.dit.isst.gtu;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,13 +10,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.upm.dit.isst.gtu.dao.CardRequestDAO;
+import es.upm.dit.isst.gtu.dao.CardRequestDAOImpl;
+import es.upm.dit.isst.gtu.model.CardRequest;
+
 public class BankServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
-		resp.setContentType("text/plain");
+
+		CardRequestDAO dao = CardRequestDAOImpl.getInstance();
+		List<CardRequest> ucr = new ArrayList<CardRequest>();
+		ucr = dao.listEntityRequests("University");
+		
+		for(CardRequest cr : ucr){
+			System.out.println(cr.getUser());
+			System.out.println(cr.getEntity());
+			System.out.println();
+		}
+
+		req.setAttribute("cards", new ArrayList<CardRequest>(ucr));
 		RequestDispatcher	view =	req.getRequestDispatcher("Bank.jsp");
 		view.forward(req,	resp);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+
+		String id = req.getParameter("id");
+		CardRequestDAO dao = CardRequestDAOImpl.getInstance();
+		dao.update(Long.parseLong(id), "Bank");
+		res.sendRedirect("/bank");
+		List<CardRequest> test = dao.listCardRequests();
+		
+		for(CardRequest cr : test){
+			System.out.println(cr.getEntity());
+		}
+
 	}
 
 }
